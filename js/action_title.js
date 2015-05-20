af51dc4025d21897aadbfed19528ac97a2c907fe
BuddyFriendGuy <bfggeneral@gmail.com>
2015-05-20 00:55:43 -0400

    add an action-title mapping

---
+++ js/action_title.js
@@ -0,0 +1,117 @@
+var action_groups = {
+
+	"movement": {
+		"+forward": "⏶",
+		"+back": "⏷",
+		"+moveleft": "⏴",
+		"+moveright": "⏵",
+
+		"+jump": "🐇",
+		"+crouch": "🐢",
+	},
+
+	"camera": {
+		"+left": "↶",
+		"+right": "↷",
+		"+roll_left": "⟲",
+		"+roll_right": "⟳",
+
+		"toggle chase_active": "3rd person view",
+	},
+
+	"program_control": {
+		"pause": "pause",
+		"disconnect": "leave",
+		"menu_showquitdialog": "quit",
+		"toggleconsole": "console",
+		"togglemenu": "menu",
+	},
+
+	"recording": {
+		"screenshot": "screenshot",
+	},
+
+	"tactic_info": {
+		"+hud_panel_radar_maximized": "radar max",
+	},
+
+	"chat": {
+		"messagemode": "chat (public)",
+		"messagemode2": "chat (team)",
+		"+con_chat_maximize": "chat history",
+	},
+
+	"game_participation": {
+		"ready": "ready",
+		"spec": "spec",
+		"menu_showteamselect": "team menu",
+		"team_auto": "auto team",
+	},
+
+	"game_info": {
+		"+showaccuracy": "accuracy",
+		"+show_info": "server info",
+		"+showscores": "scores",
+	},
+
+
+	"vote": {
+		"vno": "vote no",
+		"vyes": "vote yes",
+	},
+
+	"weapon_action": {
+		"+fire": "primary fire",
+		"+fire2": "secondary fire",
+		"reload": "reload",
+		"dropweapon": "drop weapon",
+
+		"+hook": "hook",
+		"+use": "drop item",
+		"+jetpack": "jet pack",
+		"+button8": "drag object",
+
+
+		"togglezoom": "toggle zoom",
+		"+zoom": "hold zoom",
+
+		"weapprev": "previous weapon",
+		"weapnext": "next weapon",
+		"weaplast": "last weapon",
+		"bestweapon": "best weapon",
+
+	"weapon": {
+		"weapon_group_1": "Blaster",
+		"weapon_group_2": "Shotgun",
+		"weapon_group_3": "Machine Gun",
+		"weapon_group_4": "Mortar",
+		"weapon_group_5": "Electro",
+		"weapon_group_6": "Crylink",
+		"weapon_group_7": "Vortex",
+		"weapon_group_8": "Hagar",
+		"weapon_group_9": "Devasator",
+		"weapon_group_0": "Hook",
+	},
+
+	"misc": {
+		"+userbind 1": "",
+		"+userbind 2": "",
+		"+userbind 3": "",
+		"+userbind 4": "",
+		"+userbind 6": "",
+		"+userbind 7": "",
+		"+userbind 9": "",
+		"+userbind 10": "",
+		"+userbind 11": "",
+		"+userbind 12": "",
+		"+userbind 13": "",
+		"+userbind 16": "",
+		"+userbind 17": "",
+		"+userbind 18": "",
+
+
+		"menu_showsandboxtools": "menu sandbox",
+	},
+
+
+};
